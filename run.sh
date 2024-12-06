ce052aac7fb1cccae6b3bf31b51929be25f61d9e
d3mystified <121274989+d3mystified@users.noreply.github.com>
2024-12-05 20:08:11 -0800

    fix python command in run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-nohup python3 ./src/tidal-server.py > /tmp/nohup_tidal.txt 2>&1 &
-nohup python3 ./src/index.py > /tmp/nohup_index.txt 2>&1 &
-nohup mitmdump --listen-port 8081 -s ./src/http-redirect-request.py > /tmp/nohup_mitmdump.txt 2>&1 &
+nohup python ./src/tidal-server.py > ~/nohup_tidal.txt 2>&1 &
+nohup python ./src/index.py > ~/nohup_index.txt 2>&1 &
+nohup mitmdump --listen-port 8081 -s ./src/http-redirect-request.py > ~/nohup_mitmdump.txt 2>&1 &
+
+tail -f ~/nohup_*.txt
