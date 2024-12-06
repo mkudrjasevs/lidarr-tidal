e3592e16b9c37c9dcdf5a413e14fa15720dc24da
d3mystified <121274989+d3mystified@users.noreply.github.com>
2024-12-06 12:07:53 -0800

    add sample docker-compose file, don't buffer python logs

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-nohup python ./src/tidal-server.py > ~/nohup_tidal.txt 2>&1 &
-nohup python ./src/index.py > ~/nohup_index.txt 2>&1 &
+nohup python -u ./src/tidal-server.py > ~/nohup_tidal.txt 2>&1 &
+nohup python -u ./src/index.py > ~/nohup_index.txt 2>&1 &
 nohup mitmdump --listen-port 8081 -s ./src/http-redirect-request.py > ~/nohup_mitmdump.txt 2>&1 &
 
 tail -f ~/nohup_*.txt
